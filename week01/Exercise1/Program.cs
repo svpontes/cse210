8c182e0379865c4a57e0b264c6780ed042136430
svpontes <svpontes2015@gmail.com>
2025-03-04 23:16:04 +0000

    introduce cosnole.write and console.Writeline concepts

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World!");
+        //Console.WriteLine("");
+
+        //input
+        Console.WriteLine("What is your favorite color? ");
+        string color = Console.ReadLine();
+        Console.WriteLine($"Your favorite color is: {color}");
+       
     }
 }
