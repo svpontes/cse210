d235b14938746b6b8117a0fa376dfe8c96b8cd0e
svpontes <svpontes2015@gmail.com>
2025-03-05 02:28:45 +0000

    C# Programming Exercise 2 activity

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,66 @@
 using System;
 
-class Program
-{
+class Program{
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        // ask user for a grade
+        Console.Write("What is your grade percentage? ");
+        string userGrade = Console.ReadLine();
+        int number = int.Parse(userGrade);
+
+        string letter = "";
+        string sign = "";
+
+        // Determine the grade letter
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // set signal "+" or "-"
+        int lastDigit = number % 10;
+
+        if (letter != "F") // Avoid F+ and F-
+        {
+            if (lastDigit >= 7 && letter != "A") // avoid A +
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+
+        // show results
+        Console.WriteLine($"Your grade is {letter}{sign}");
+
+        // verify student status
+        if (number >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately, you did not pass! Do not give up! Keep pursuing your goals!");
+        }
     }
 }
