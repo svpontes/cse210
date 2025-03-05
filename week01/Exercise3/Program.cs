c5234d67dc3e33e1c118bd31b6eb57bd57432aad
svpontes <svpontes2015@gmail.com>
2025-03-05 16:46:29 +0000

    solution for exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,51 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        //while loops, do-while, for, foreach
+        //Program guess a number
+
+        int gNumber; //guess number
+        //int mNumber; //magic number
+       
+        //mNumber = int.Parse(Console.ReadLine());
+        int count = 0;
+        bool playAgain = true;
+        
+        while(playAgain){ //nested loop
+
+            Random randomGenerator = new Random();//create a instance 
+            int rNumber = randomGenerator.Next(1, 100);//generate a range of numbers
+            Console.Write("Guess Number has been Generated!"); 
+
+                do{
+                Console.Write(" What is your guess? ");
+                gNumber = int.Parse(Console.ReadLine());
+                
+                if(gNumber > rNumber){
+                    Console.WriteLine("Lower");
+                    }
+                    else if (gNumber < rNumber){
+                        Console.WriteLine("Higher");
+                    }
+                else{
+                    Console.WriteLine("");
+                    Console.WriteLine("Congratulations! You guess it!");
+                    
+                }
+                count ++;
+                        
+                
+                } while(rNumber != gNumber);
+
+            Console.WriteLine($"You guess {count} times!");
+            Console.WriteLine("Do you want to play again? Y (Yes) N (no)");
+            string response = Console.ReadLine().ToLower();
+            playAgain = (response == "y");
+
+            
+        }
+        
+           
     }
 }
