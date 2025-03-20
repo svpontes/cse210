141539fbe870f60e85945daefebfe55bd50e9281
svpontes <svpontes2015@gmail.com>
2025-03-20 14:31:51 +0000

    testing 2 instances of Reference with outputs: //expected output:  [Book of Mormon] First Nefi 3: 7 and //expected output:  [Book of Mormon] First Nefi 3: 7-8

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -8,7 +8,10 @@ class Program
 
         //testing method DisplayReference(string book, string bookName, int chapter, int verse, int endVerse)
 
-        Reference ref1 = new Reference("Book of Mormon", "First Nefi", 3, 7, 8);//
-        Console.WriteLine(ref1.DisplayReference());
+        Reference ref1 = new Reference("Book of Mormon", "First Nefi", 3, 7);//No endVerse
+        Console.WriteLine(ref1.DisplayReference());//expected output:  [Book of Mormon] First Nefi 3: 7
+
+        Reference ref2 = new Reference("Book of Mormon", "First Nefi", 3, 7, 8);//With endVerse
+        Console.WriteLine(ref2.DisplayReference());//expected output:  [Book of Mormon] First Nefi 3: 7-8
     }
 }
