8077fd93f4f2bee5393103080ea0f20069423018
svpontes <svpontes2015@gmail.com>
2025-03-20 14:27:44 +0000

    change aproach for endVerse variable member. Set -1 instead of int? (null). This way it is not necessary to pass value null as parameter when calling the DisplayReference method

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+
+        //testing method DisplayReference(string book, string bookName, int chapter, int verse, int endVerse)
+
+        Reference ref1 = new Reference("Book of Mormon", "First Nefi", 3, 7, 8);//
+        Console.WriteLine(ref1.DisplayReference());
     }
 }
--- week03/ScriptureMemorizer/Reference.cs
+++ week03/ScriptureMemorizer/Reference.cs
@@ -1,14 +1,16 @@
 public class Reference{
 
-    string _book;
-    int _chapter;
-    int _verse;
-    int? _endVerse;//int? allow null if the scripture reference has one verse. 
+    private string _book;//Bible, Book of Mormon, Doctrines and Convenants, Pearl of Gret Price 
+    private string _bookName;//ex: First Nefi
+    private int _chapter;
+    private int _verse;
+    private int _endVerse;//allow checking for one verse or more verses
 
     //method constructor 
-    public Reference(string book, int chapter, int verse, int? endVerse){
+    public Reference(string book, string bookName, int chapter, int verse, int endVerse = -1){//set -1 to endVerse in case of there is no endVerse. 
 
         _book = book;
+        _bookName = bookName;
         _chapter = chapter;
         _verse = verse;
         _endVerse = endVerse;
@@ -16,12 +18,12 @@ public class Reference{
     //method to display book, chapter, verses
     public string DisplayReference(){
 
-        if (_endVerse.HasValue){
+        if (_endVerse != -1){
 
-            return $"[{_book}] [{_chapter}: {_verse} - {_endVerse}]";//output display format for an interval of verses "-"
+            return $"[{_book}] {_bookName} {_chapter}: {_verse}-{_endVerse}";//output display format for an interval of verses "-"
         }
         else{
-            return $"[{_book}] [{_chapter}: {_verse}]";//output display format for one verse
+            return $"[{_book}] {_bookName} {_chapter}: {_verse}";//output display format for one verse
         }
     }
 }
